044bf7702f7dda92eb2528e3d3eb5433efe13b31
DCowboy <demoncowboy@hotmail.com>
2020-06-20 18:50:34 -0400

    restructured save and load to stage possible new save option.

--- addons/godot_db_manager/db_interface.gd
+++ addons/godot_db_manager/db_interface.gd
@@ -1,144 +1,318 @@
-"""
-class GDDBInterface
-"""
-
-class_name GDDBInterface
-
 tool
 extends Control
 
-var m_db_manager = null
+enum {
+	e_action_none = -1
+	e_action_new_db = 0,
+	e_action_load_db = 1
+}
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# init db_manager
-	m_db_manager = load(gddb_constants.c_addon_main_path + "core/db_man.gd").new()
+const default_db_name = "database"
 
-	# menu connections
-	$dlg/menu.connect("new_database", self, "on_menu_new_database")
-	$dlg/menu.connect("load_database", self, "on_menu_load_database")
-	$dlg/menu.connect("save_database", self, "on_menu_save_database")
-	$dlg/menu.connect("save_database_as", self, "on_menu_save_database_as")
+var m_current_db_name = ""
 
-	# new database connections
-	$dlg/new_db_dlg.connect("create_new_db", self, "on_new_database")
+var m_current_table_idx = -1
+var m_tables = []
 
-	# save / load connections
+var m_ctrl_pressed = false
+
+var m_is_saving = false
+var m_closing = false
+
+var m_autosave_on_close_enabled = false
+
+var m_action = e_action_none
+
+func _ready():
+	m_action = e_action_none
+	m_current_db_name = ""
+	setup_connections()
+	update_buttons()
+
+func setup_connections():
+	$dlg/menu/new_db_btn.connect("pressed", self, "on_new_database")
+	$dlg/new_db_dlg.add_cancel("Cancel")
+	$dlg/new_db_dlg.connect("create_new_db", self, "on_create_db")
+
+	$dlg/confirm_new_db_dlg.connect("confirm_new_database", self, "on_confirm_new_database")
+
+	$dlg/menu/save_db_btn.connect("pressed", self, "on_save_database_btn_pressed")
+	$dlg/menu/load_db_btn.connect("pressed", self, "on_load_database_btn_pressed")
+	$dlg/menu/new_table_btn.connect("pressed", self, "on_new_table_btn_pressed")
+	$dlg/menu/autosave_on_load.connect("toggled", self, "on_toggle_autosave_on_close")
+
+	$dlg/new_table_dlg.add_cancel("Cancel")
+	$dlg/new_table_dlg.connect("create_new_table", self, "on_table_created")
+
+	$dlg/tables_container.connect("item_selected", self, "on_select_table")
+
+	$dlg/table.connect("new_property", self, "on_new_property")
+	$dlg/table.connect("change_property", self, "on_change_property")
+	$dlg/table.connect("delete_property", self, "on_delete_property")
+	$dlg/table.connect("new_data_row", self, "on_new_data_row")
+	$dlg/table.connect("update_data", self, "on_update_data")
+
 	$dlg/load_db_dlg.connect("file_selected", self, "on_file_selected")
 
-# called when creating a new database from the menu
-func on_menu_new_database() -> void:
-	$dlg/new_db_dlg/v_layout/db_info/db_edt.set_text("")
-	$dlg/new_db_dlg.popup_centered()
+	$dlg.connect("popup_hide", self, "on_close_dlg")
 
-# called when loading a database from the menu
-func on_menu_load_database() -> void:
-	$dlg/load_db_dlg.set_mode(FileDialog.MODE_OPEN_FILE)
-	$dlg/load_db_dlg.set_title("Load Database ...")
-	$dlg/load_db_dlg.set_current_file("")
-	$dlg/load_db_dlg.popup_centered()
+func update_buttons():
+	var disable_buttons = m_current_db_name.empty()
+	$dlg/menu/save_db_btn.set_disabled(disable_buttons)
+	$dlg/menu/autosave_on_load.set_disabled(disable_buttons)
+	$dlg/menu/new_table_btn.set_disabled(disable_buttons)
 
-# called when saving a database from the menu
-func on_menu_save_database() -> void:
-	# print("on_menu_save_database")
-	var currnet_tab = $dlg/databases.get_current_tab_control()
-	if(currnet_tab.can_save_database()):
-		currnet_tab.save_database()
+func _input(event):
+	if(event is InputEventKey):
+		if(event.scancode == KEY_CONTROL):
+			m_ctrl_pressed = event.is_pressed()
+		elif(event.scancode == KEY_S):
+			if(!m_current_db_name.empty()):
+				if(event.is_pressed()):
+					if(m_ctrl_pressed):
+						if(!m_is_saving):
+							m_is_saving = true
+							on_save_database_btn_pressed()
+					else:
+						m_is_saving = false
+				else:
+					m_is_saving = false
+			else:
+				m_is_saving = false
+		elif(event.scancode == KEY_F4):
+			if(event.is_pressed()):
+				if(m_ctrl_pressed):
+					if(!m_closing):
+						m_closing = true
+						$dlg.hide()
+				else:
+					m_closing = false
+			else:
+				m_closing = false
+
+func on_new_database():
+	if(!m_current_db_name.empty()):
+		m_action = e_action_new_db
+		$dlg/confirm_new_db_dlg.popup_centered()
 	else:
-		on_menu_save_database_as()
+		$dlg/new_db_dlg/db_info/db_edt.set_text("")
+		$dlg/new_db_dlg.popup_centered()
 
-# called when saving a database as another from the menu
-func on_menu_save_database_as():
-	# print("on_menu_save_database_as")
-	$dlg/load_db_dlg.set_mode(FileDialog.MODE_SAVE_FILE)
-	$dlg/load_db_dlg.set_title("Save Database As ...")
+func on_confirm_new_database():
+	if(m_action == e_action_new_db):
+		$dlg/new_db_dlg/db_info/db_edt.set_text("")
+		$dlg/new_db_dlg.popup_centered()
+	else:
+		$dlg/load_db_dlg.popup_centered()
 
-	var currnet_tab = $dlg/databases.get_current_tab_control()
+func on_create_db(db_name):
+	m_current_db_name = "res://"
+	if(db_name.ends_with(".json")):
+		m_current_db_name += db_name
+	else:
+		m_current_db_name += db_name + ".json"
+		$dlg/menu/current_db_name.set_text("DB path: " + m_current_db_name)
+	update_buttons()
+	m_tables.clear()
+	$dlg/tables_container.clear()
+	$dlg/table.clear_current()
+	$dlg/table.hide()
 
-	$dlg/load_db_dlg.set_current_file(currnet_tab.get_db_name())
-	$dlg/load_db_dlg.popup_centered()
+func on_save_database_btn_pressed():
+	var database = {}
 
-# called when adding a new database
-func on_new_database(db_name : String) -> void:
-	var tmp_name = db_name.to_lower()
-	var db_id = m_db_manager.add_database(db_name)
-	if(db_id == gddb_constants.c_invalid_id):
-		$dlg/error_dlg.set_text("Database with name \"" + db_name + "\" already exists")
-		$dlg/error_dlg.popup_centered()
-		return
+	for idx in range(0, m_tables.size()):
+		database[str(m_tables[idx].get_name())] = {}
+		for jdx in range(0, m_tables[idx].get_props_count()):
+			database[m_tables[idx].get_name()][str(m_tables[idx].get_prop_name(jdx))] = m_tables[idx].get_data_at(jdx)
 
-	var db = m_db_manager.get_db_by_id(db_id)
-	# print("new DB added: " + str(db))
-
-	var db_editor = load(gddb_constants.c_addon_main_path + "db_editor.tscn").instance()
-	$dlg/databases.add_child(db_editor)
-	db_editor.set_name(db_name)
-	db.set_dirty(true)
-	db_editor.set_database(db)
-
-	$dlg/menu.enable_file_save(true)
-	$dlg/menu.enable_file_save_as(true)
-
-	$dlg/new_db_dlg.hide()
-
-# called when selecting a file from save / load dialog
-func on_file_selected(filepath : String) -> void:
-	# print("GDDBInterface::on_file_selected(" + filepath + ")")
-
-	if($dlg/load_db_dlg.get_mode() == FileDialog.MODE_SAVE_FILE):
-		var filepath_low = filepath.to_lower()
-
-		# check for the file extension
-		if(!filepath_low.ends_with(".json")):
-			$dlg/error_dlg.set_text("The extension of the file must be \".json\"")
-			$dlg/error_dlg.popup_centered()
-			return
+	var save_file = File.new()
+	save_file.open(m_current_db_name, File.WRITE)
+	for each in database:
+#		save_file.store_string("Table: " + str(each))
+		save_file.store_line(to_json(database[each]))
+	save_file.close()
 	
-		var current_filename = $dlg/load_db_dlg.get_current_file().to_lower()
-		var filename_dot = current_filename.length() - 5
-		current_filename.erase(filename_dot, 5)
-	
-		# check the filename
-		if(!gddb_globals.check_db_name(current_filename)):
-			$dlg/error_dlg.set_text("Invalid characters in the database filename.\n\nThe filename cannot contain any of these characters: " + gddb_constants.c_invalid_characters)
-			$dlg/error_dlg.popup_centered()
-			return
 
-		save_database_as(filepath)
+#	var text = "{"
+#	text += "\"tables\":["
+#	for idx in range(0, m_tables.size()):
+#		text += "{"
+#		text += "\"name\":\"" + m_tables[idx].get_name() + "\","
+#		text += "\"props\":["
+#		for jdx in range(0, m_tables[idx].get_props_count()):
+#			text += "{"
+#			text += "\"name\":\"" + str(m_tables[idx].get_prop_name(jdx)) + "\","
+#			text += "\"type\":\"" + str(m_tables[idx].get_prop_type(jdx)) + "\""
+#			text += "}"
+#			if(jdx < m_tables[idx].get_props_count() - 1):
+#				text += ","
+#		text += "],"
+#
+#		text += "\"data\":["
+#		for jdx in range(0, m_tables[idx].get_data_size()):
+#			#var the_data = m_tables[idx].get_data_at(jdx)
+#			#print("getting data at " + str(jdx) + " : " + the_data)
+#			text += "\"" + m_tables[idx].get_data_at(jdx) + "\""
+#			if(jdx < m_tables[idx].get_data_size() - 1):
+#				text += ","
+#		text += "]" # end of data
+#		text += "}" # end of table
+#
+#		if(idx < m_tables.size() - 1):
+#			text += ","
+#
+#	text += "]}"
+#
+#	var save_file = File.new()
+#	save_file.open(m_current_db_name, File.WRITE)
+#	save_file.store_string(text)
+#	save_file.close()
 
-	elif($dlg/load_db_dlg.get_mode() == FileDialog.MODE_OPEN_FILE):
-		load_database(filepath)
 
-# saves a database to a given file path
-func save_database_as(filepath : String) -> void:
-	# print("GDDBInterface::save_database_as(" + filepath + ")")
-	var currnet_tab = $dlg/databases.get_current_tab_control()
-	currnet_tab.set_database_filepath(filepath)
-	currnet_tab.save_database()
+func on_load_database_btn_pressed():
+	if(!m_current_db_name.empty()):
+		m_action = e_action_load_db
+		$dlg/confirm_new_db_dlg.popup_centered()
+	else:
+		$dlg/load_db_dlg.popup_centered()
 
-func load_database(filepath : String) -> void:
-	var db_id = m_db_manager.load_database(filepath)
+func on_toggle_autosave_on_close(btn_pressed):
+	m_autosave_on_close_enabled = btn_pressed
 
-	if(db_id == gddb_types.e_db_invalid_file):
-		$dlg/error_dlg.set_text("Invalid database")
+func on_close_dlg():
+	if(m_autosave_on_close_enabled):
+		# automatic save DB when closing the dialog
+		on_save_database_btn_pressed()
+
+func on_file_selected(file_path):
+	m_current_db_name = file_path
+	$dlg/menu/current_db_name.set_text("DB path: " + file_path)
+	var file = File.new()
+	file.open(file_path, File.READ)
+	var content = file.get_as_text()
+	file.close()
+	var dictionary = JSON.parse(content).result
+
+	# refresh tables in the database
+	m_tables.clear()
+	$dlg/tables_container.clear()
+	var tables = dictionary["tables"]
+	for idx in range(0, tables.size()):
+		$dlg/tables_container.add_item(tables[idx]["name"])
+
+		var table = load("res://addons/godot_db_manager/db_table.gd").new()
+		table.set_name(tables[idx]["name"])
+		m_tables.push_back(table)
+
+		var props_count = tables[idx]["props"].size()
+		if(props_count == 0):
+			continue
+
+		for jdx in range(0, props_count):
+			table.add_prop(jdx, int(tables[idx]["props"][jdx]["type"]), tables[idx]["props"][jdx]["name"])
+
+		var data_count = tables[idx]["data"].size()
+		for jdx in range(0, data_count / props_count):
+			var row_data = []
+			for kdx in range(0, props_count):
+				row_data.push_back(tables[idx]["data"][jdx * props_count + kdx])
+			table.add_row(jdx, row_data)
+
+	# refresh interface
+	$dlg/table.show()
+	m_current_table_idx = 0
+	$dlg/tables_container.select(m_current_table_idx)
+	if(m_tables.size() > 0):
+		$dlg/table.set_table(m_tables[m_current_table_idx])
+
+	update_buttons()
+
+func deserialize_database(dictionary):
+	var tables = dictionary["tables"]
+	for idx in range(0, tables.size()):
+		print(tables[idx])
+
+func on_new_table_btn_pressed():
+	$dlg/new_table_dlg/table_info/table_edt.set_text("")
+	$dlg/new_table_dlg.popup_centered()
+
+func on_table_created(table_name):
+	if(!can_create_table(table_name)):
+		$dlg/new_table_dlg/table_info/table_edt.set_text("")
+		$dlg/new_table_dlg.popup_centered()
+		$dlg/error_dlg.set_text("Table with name \"" + table_name + "\" already exists !")
 		$dlg/error_dlg.popup_centered()
 		return
 
-	if(db_id == gddb_types.e_db_invalid_ver):
-		$dlg/error_dlg.set_text("Wrong database version. Currently is: " + gddb_constants.c_gddb_ver)
-		$dlg/error_dlg.popup_centered()
+	m_current_table_idx = $dlg/tables_container.get_item_count()
+
+	$dlg/tables_container.add_item(table_name)
+	$dlg/tables_container.select(m_current_table_idx)
+
+	var table = load("res://addons/godot_db_manager/db_table.gd").new()
+	table.set_name(table_name)
+	m_tables.push_back(table)
+
+	on_select_table(m_current_table_idx)
+	$dlg/table.show()
+
+func can_create_table(table_name):
+	for idx in range(0, m_tables.size()):
+		if(m_tables[idx].get_name() == table_name):
+			return false
+	return true
+
+func on_select_table(table_idx):
+	if(table_idx < 0 || table_idx >= m_tables.size()):
+		print("ERROR: database::on_select_table( " + str(table_idx) + " ) havinng " + str(m_tables.size()) + " tables count")
 		return
+	# print("Selecting: " + $tables/HBoxContainer/ItemList.get_item_text(table_idx))
+	m_current_table_idx = table_idx
+	$dlg/table.set_table(m_tables[m_current_table_idx])
 
-	var db = m_db_manager.get_db_by_id(db_id)
-	db.set_dirty(false)
-	# print("new DB added: " + str(db))
+func on_new_property(prop_id, prop_type, prop_name):
+	if(m_current_table_idx < 0 || m_current_table_idx >= m_tables.size()):
+		print("ERROR: database::on_new_property( " + str(prop_id) + ", " + str(prop_type) + ", " + prop_name + " ) - wrong m_current_table_idx: " + str(m_current_table_idx) + " - tables count: " + str(m_tables.size()))
+		return
+	m_tables[m_current_table_idx].add_prop(prop_id, prop_type, prop_name)
 
-	var db_editor = load(gddb_constants.c_addon_main_path + "db_editor.tscn").instance()
-	$dlg/databases.add_child(db_editor)
-	db_editor.set_name(db.get_db_name())
-	db_editor.set_database(db)
+func on_change_property(prop_id, prop_type, prop_name):
+	if(m_current_table_idx < 0 || m_current_table_idx >= m_tables.size()):
+		print("ERROR: database::on_change_property( " + str(prop_id) + ", " + str(prop_type) + ", " + prop_name + " ) - wrong m_current_table_idx: " + str(m_current_table_idx) + " - tables count: " + str(m_tables.size()))
+		return
+	m_tables[m_current_table_idx].change_prop(prop_id, prop_type, prop_name)
 
-	$dlg/menu.enable_file_save(true)
-	$dlg/menu.enable_file_save_as(true)
+func on_delete_property(prop_id):
+	if(m_current_table_idx < 0 || m_current_table_idx >= m_tables.size()):
+		print("ERROR: database::on_change_property( " + str(prop_id) + " ) - wrong m_current_table_idx: " + str(m_current_table_idx) + " - tables count: " + str(m_tables.size()))
+		return
+	m_tables[m_current_table_idx].delete_prop(prop_id)
 
-	db_editor.set_dirty(false)
+func on_new_data_row():
+	if(m_current_table_idx < 0 || m_current_table_idx >= m_tables.size()):
+		print("ERROR: database::on_new_data_row() - wrong m_current_table_idx: " + str(m_current_table_idx) + " - tables count: " + str(m_tables.size()))
+		return
+	m_tables[m_current_table_idx].add_blank_row()
+
+func get_tables_count():
+	return m_tables.size()
+
+func get_table_by_name(name):
+	for idx in range(0, m_tables.size()):
+		if(m_tables[idx].get_name() == name):
+			return m_tables[idx]
+	print("ERROR: database::get_table_by_name( " + name + " ) not found")
+	return null
+
+func get_table_by_idx(idx):
+	if(idx < 0 || idx > m_tables.size()):
+		print("ERROR: database::get_table_by_idx( " + str(idx) + " )")
+		return null
+	return m_tables[idx]
+
+func get_table_name(idx):
+	return m_tables[idx].get_name()
+
+func on_update_data(prop_id, row_idx, data):
+	m_tables[m_current_table_idx].update_data(prop_id, row_idx, data)
